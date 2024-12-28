6f4393e25672eb8d106dac6516d0cb3a3ccf0914
gab-braga <gabrielbraga0712@gmail.com>
2024-12-27 22:40:31 -0300

    feat: login page created and style added

--- perch-search-client/src/main/java/io/github/gabbraga/App.java
+++ perch-search-client/src/main/java/io/github/gabbraga/App.java
@@ -2,16 +2,7 @@ package io.github.gabbraga;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -19,26 +10,26 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class App implements EntryPoint {
 
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
 
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Greetings Earthling");
-		final TextBox nameField = new TextBox();
-		final Label errorLabel = new Label();
+		final Label titleLabel = new Label("Login");
+		final Label emailLabel = new Label("E-mail");
+		final Label passLabel = new Label("Senha");
+		final TextBox emailField = new TextBox();
+		final TextBox passField = new TextBox();
+		final Button sendButton = new Button("Entrar");
 		final VerticalPanel  vertPanel = new VerticalPanel();
 
-		nameField.setText("GWT User");
+		titleLabel.addStyleName("titleLabel");
 
-		sendButton.addStyleName("sendButton");
-
-		vertPanel.add(nameField);
+		vertPanel.add(titleLabel);
+		vertPanel.add(emailLabel);
+		vertPanel.add(emailField);
+		vertPanel.add(passLabel);
+		vertPanel.add(passField);
 		vertPanel.add(sendButton);
-		vertPanel.add(errorLabel);
 
 		RootPanel.get().add(vertPanel);
 	}
